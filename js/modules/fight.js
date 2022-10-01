73794bc2ae5ba04b4ae6f98c31d07e1be39b05cd
lidrial <remy-nicolas@live.fr>
2022-10-01 14:08:55 +0200

    await api

--- js/modules/fight.js
+++ js/modules/fight.js
@@ -10,41 +10,41 @@ function getRandom(max){
 
 }
 
-function getOnePokemon(){
-    let pkmn_sauvage = getRandom(151)
-    console.log('pkmnsauvage', pkmn_sauvage)
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pkmn_sauvage}/`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        localStorage.setItem('pkmn_sauvage_details',JSON.stringify([data.name, data.moves, data.stats, data.sprites]))
-        console.log(JSON.parse(localStorage.getItem('pkmn_sauvage_details')))
-    })
+async function getOnePokemon(){
+    let pkmn_sauvageID = getRandom(151)
+    console.log('pkmnsauvage', pkmn_sauvageID)
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pkmn_sauvageID}/`)
+    let pkmn_sauvage = await response.json()
+
+    localStorage.setItem('pkmn_sauvage_details',JSON.stringify([pkmn_sauvage.name, pkmn_sauvage.moves, pkmn_sauvage.stats, pkmn_sauvage.sprites]))
+
+    console.log('jsonsauvage', pkmn_sauvage);
+
+    return pkmn_sauvage
 }
 
-function getPlayerPokemon(){
-    const player_pkmn = localStorage.starter_pkmn
-    fetch(`https://pokeapi.co/api/v2/pokemon/${player_pkmn}/`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        localStorage.setItem('pkmn_player_details',JSON.stringify([data.name, data.moves, data.stats, data.sprites]))
-        console.log(JSON.parse(localStorage.getItem('pkmn_player_details')))
-        // console.log(JSON.parse(localStorage.getItem('pkmn_sauvage_details'[3])))
+async function getPlayerPokemon(){
+    const player_pkmnID = localStorage.starter_pkmn
+    let response = await (fetch(`https://pokeapi.co/api/v2/pokemon/${player_pkmnID}/`))
+    let player_pkmn = await response.json()
 
-    })
+    localStorage.setItem('pkmn_player_details',JSON.stringify([player_pkmn.name, player_pkmn.moves, player_pkmn.stats, player_pkmn.sprites]))
+
+    console.log('jsonplayer', player_pkmn);
+
+    return player_pkmn
 
 }
 
-export function fight(){
+export async function fight(){
     if ((getRandom(3)) == 0 ){
         fightdiv.classList.toggle('is-hidden')
         document.getElementById('page').classList.toggle('grayscale')
-        getOnePokemon()
+        await getOnePokemon()
         var sauvage_sprites = (JSON.parse(localStorage.getItem('pkmn_sauvage_details')))[3].front_default
         //var sauvage_sprites = (localStorage.pkmn_sauvage_details)[3].front_default
         enemy.innerHTML += '<img src="' + sauvage_sprites + '"' + 'id="sauvage_img">'
-        getPlayerPokemon()
+        await getPlayerPokemon()
         var player_sprites = (JSON.parse(localStorage.getItem('pkmn_player_details')))[3].back_default
         you.innerHTML += '<img src="' + player_sprites + '"' + 'id="player_img">'
 
